d5bb30218188357b1768fc15d57c23dd4ec3fe0a
MaxXSoft <x@MaxXSoft.net>
2019-09-14 23:06:36 +0800

    (Core) added port definitions

--- src/core/Core.v
+++ src/core/Core.v
@@ -1,8 +1,27 @@
 `timescale 1ns / 1ps
 
+`include "bus.v"
+
 module Core(
-  input clk,
-  input rst,
+  input                   clk,
+  input                   rst,
+  // ROM control
+  output                  rom_en,
+  output  [`MEM_SEL_BUS]  rom_write_en,
+  output  [`ADDR_BUS]     rom_addr,
+  input   [`DATA_BUS]     rom_read_data,
+  output  [`DATA_BUS]     rom_write_data,
+  // RAM control
+  output                  ram_en,
+  output  [`MEM_SEL_BUS]  ram_write_en,
+  output  [`ADDR_BUS]     ram_addr,
+  input   [`DATA_BUS]     ram_read_data,
+  output  [`DATA_BUS]     ram_write_data,
+  // debug signals
+  output                  debug_reg_write_en,
+  output  [`REG_ADDR_BUS] debug_reg_write_addr,
+  output  [`DATA_BUS]     debug_reg_write_data,
+  output  [`ADDR_BUS]     debug_pc_addr
 );
 
   //
