20fe71adbf11384f42f72b8865692cf265304592
MaxXSoft <x@MaxXSoft.net>
2019-09-14 20:00:55 +0800

    (ID, FunctGen) added ID stage

---
+++ src/core/id/FunctGen.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+`include "bus.v"
+`include "opcode.v"
+`include "funct.v"
+
+module FunctGen(
+  input   [`INST_OP_BUS]  op,
+  input   [`FUNCT_BUS]    funct_in,
+  input   [`REG_ADDR_BUS] rt,
+  output  [`FUNCT_BUS]    funct
+);
+
+  reg[`FUNCT_BUS] funct;
+
+  // generating FUNCT signal in order for the ALU to perform operations
+  always @(*) begin
+    case (op)
+      `OP_SPECIAL: funct <= funct_in;
+      `OP_LUI: funct <= `FUNCT_OR;
+      `OP_LB, `OP_LBU, `OP_LW,
+      `OP_SB, `OP_SW, `OP_ADDIU: funct <= `FUNCT_ADDU;
+      `OP_JAL: funct <= `FUNCT_OR;
+      default: funct <= `FUNCT_NOP;
+    endcase
+  end
+
+endmodule // FunctGen
