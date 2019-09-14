ae9e38dc05bec0b71052144f672c7f40db988ce0
MaxXSoft <x@MaxXSoft.net>
2019-09-14 22:14:20 +0800

    (ex) added EX stage

---
+++ src/core/ex/EX.v
@@ -0,0 +1,88 @@
+`timescale 1ns / 1ps
+
+`include "bus.v"
+`include "funct.v"
+
+module EX(
+  // from ID stage
+  input   [`FUNCT_BUS]    funct,
+  input   [`SHAMT_BUS]    shamt,
+  input   [`DATA_BUS]     operand_1,
+  input   [`DATA_BUS]     operand_2,
+  input                   mem_read_flag_in,
+  input                   mem_write_flag_in,
+  input                   mem_sign_ext_flag_in,
+  input   [`MEM_SEL_BUS]  mem_sel_in,
+  input   [`DATA_BUS]     mem_write_data_in,
+  input                   reg_write_en_in,
+  input   [`REG_ADDR_BUS] reg_write_addr_in,
+  input   [`ADDR_BUS]     current_pc_addr_in,
+  // to ID stage (solve data hazards)
+  output                  ex_load_flag,
+  // to MEM stage
+  output                  mem_read_flag_out,
+  output                  mem_write_flag_out,
+  output                  mem_sign_ext_flag_out,
+  output  [`MEM_SEL_BUS]  mem_sel_out,
+  output  [`DATA_BUS]     mem_write_data_out,
+  // to WB stage
+  output  [`DATA_BUS]     result,
+  output                  reg_write_en_out,
+  output  [`REG_ADDR_BUS] reg_write_addr_out,
+  output  [`ADDR_BUS]     current_pc_addr_out
+);
+
+  // to ID stage
+  assign ex_load_flag = mem_read_flag_in;
+  // to MEM stage
+  assign mem_read_flag_out = mem_read_flag_in;
+  assign mem_write_flag_out = mem_write_flag_in;
+  assign mem_sign_ext_flag_out = mem_sign_ext_flag_in;
+  assign mem_sel_out = mem_sel_in;
+  assign mem_write_data_out = mem_write_data_in;
+  // to WB stage
+  assign reg_write_en_out = !mem_write_flag_in;
+  assign reg_write_addr_out = reg_write_addr_in;
+  assign current_pc_addr_out = current_pc_addr_in;
+
+  // calculate the complement of operand_2
+  wire[`DATA_BUS] operand_2_mux =
+      (funct == `FUNCT_SUBU || funct == `FUNCT_SLT)
+        ? (~operand_2) + 1 : operand_2;
+
+  // sum of operand_1 & operand_2
+  wire[`DATA_BUS] result_sum = operand_1 + operand_2_mux;
+
+  // flag of operand_1 < operand_2    
+  wire operand_1_lt_operand_2 = funct == `FUNCT_SLT ?
+        // op1 is negative & op2 is positive
+        ((operand_1[31] && !operand_2[31]) ||
+          // op1 & op2 is positive, op1 - op2 is negative
+          (!operand_1[31] && !operand_2[31] && result_sum[31]) ||
+          // op1 & op2 is negative, op1 - op2 is negative
+          (operand_1[31] && operand_2[31] && result_sum[31]))
+      : (operand_1 < operand_2);
+
+  // calculate result
+  reg[`DATA_BUS] result;
+
+  always @(*) begin
+    case (funct)
+      // jump with link & logic
+      `FUNCT_JALR, `FUNCT_OR: result <= operand_1 | operand_2;
+      `FUNCT_AND: result <= operand_1 & operand_2;
+      `FUNCT_XOR: result <= operand_1 ^ operand_2;
+      // comparison
+      `FUNCT_SLT, `FUNCT_SLTU: result <= {31'b0, operand_1_lt_operand_2};
+      // arithmetic
+      `FUNCT_ADDU, `FUNCT_SUBU: result <= result_sum;
+      // shift
+      `FUNCT_SRA: result <= ({32{operand_2[31]}} << (6'd32 - {1'b0, shamt})) | operand_2 >> shamt;
+      `FUNCT_SLLV: result <= operand_2 << operand_1[4:0];
+      `FUNCT_SRLV: result <= operand_2 >> operand_1[4:0];
+      `FUNCT_SRAV: result <= ({32{operand_2[31]}} << (6'd32 - {1'b0, operand_1[4:0]})) | operand_2 >> operand_1[4:0];
+      default: result <= 0;
+    endcase
+  end
+
+endmodule // EX
---
+++ src/core/ex/EXMEM.v
@@ -0,0 +1,87 @@
+`timescale 1ns / 1ps
+
+`include "bus.v"
+
+module EXMEM(
+  input clk,
+  input rst,
+  input stall_current_stage,
+  input stall_next_stage,
+  // input from EX stage
+  input mem_read_flag_in,
+  input mem_write_flag_in,
+  input mem_sign_ext_flag_in,
+  input [`MEM_SEL_BUS] mem_sel_in,
+  input [`DATA_BUS] mem_write_data_in,
+  input [`DATA_BUS] result_in,
+  input  reg_write_en_in,
+  input [`REG_ADDR_BUS] reg_write_addr_in,
+  input [`ADDR_BUS] current_pc_addr_in,
+  // output to MEM stage
+  output mem_read_flag_out,
+  output mem_write_flag_out,
+  output mem_sign_ext_flag_out,
+  output [`MEM_SEL_BUS] mem_sel_out,
+  output [`DATA_BUS] mem_write_data_out,
+  // output to WB stage
+  output [`DATA_BUS] result_out,
+  output reg_write_en_out,
+  output [`REG_ADDR_BUS] reg_write_addr_out,
+  output [`ADDR_BUS] current_pc_addr_out
+);
+
+  PipelineDeliver #(1) ff_mem_read_flag(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    mem_read_flag_in, mem_read_flag_out
+  );
+
+  PipelineDeliver #(1) ff_mem_write_flag(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    mem_write_flag_in, mem_write_flag_out
+  );
+
+  PipelineDeliver #(1) ff_mem_sign_ext_flag(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    mem_sign_ext_flag_in, mem_sign_ext_flag_out
+  );
+
+  PipelineDeliver #(`MEM_SEL_BUS_WIDTH) ff_mem_sel(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    mem_sel_in, mem_sel_out
+  );
+
+  PipelineDeliver #(`DATA_BUS_WIDTH) ff_mem_write_data(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    mem_write_data_in, mem_write_data_out
+  );
+
+  PipelineDeliver #(`DATA_BUS_WIDTH) ff_result(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    result_in, result_out
+  );
+
+  PipelineDeliver #(1) ff_reg_write_en(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    reg_write_en_in, reg_write_en_out
+  );
+
+  PipelineDeliver #(`REG_ADDR_BUS_WIDTH) ff_reg_write_addr(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    reg_write_addr_in, reg_write_addr_out
+  );
+
+  PipelineDeliver #(`ADDR_BUS_WIDTH) ff_current_pc_addr(
+    clk, rst,
+    stall_current_stage, stall_next_stage,
+    current_pc_addr_in, current_pc_addr_out
+  );
+
+endmodule // EXMEM
