e3de85e8d139801f52e2381e6b92547cbe39ec6b
MaxXSoft <x@MaxXSoft.net>
2019-09-16 10:03:05 +0800

    (sim) increased the size of instruction ROM

--- src/include/sim.v
+++ src/include/sim.v
@@ -5,7 +5,7 @@
 `define DATA_MEM_SIZE         2 ** `DATA_MEM_ADDR_WIDTH
 `define DATA_MEM_BUS          `DATA_MEM_SIZE - 1:0
 
-`define INST_MEM_ADDR_WIDTH   6
+`define INST_MEM_ADDR_WIDTH   10
 `define INST_MEM_SIZE         2 ** `INST_MEM_ADDR_WIDTH
 `define INST_MEM_BUS          `INST_MEM_SIZE - 1:0
 
