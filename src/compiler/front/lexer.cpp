8173273c9059038d40721f5b38ee6855df6ba66b
MaxXSoft <x@MaxXSoft.net>
2019-09-17 19:20:00 +0800

    (lexer) added class 'Lexer'

---
+++ src/compiler/front/lexer.cpp
@@ -0,0 +1,264 @@
+#include "front/lexer.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+
+#include "util/style.h"
+
+using namespace tinylang::front;
+
+namespace {
+
+enum class NumberType {
+  Normal, Hex, Bin,
+};
+
+const char *keywords[] = {
+  "var", "let", "def", "as",
+  "i32", "i8", "i32*", "i8*",
+  "ui32", "ui8", "ui32*", "ui8*",
+  "if", "else", "while",
+  "break", "continue", "return",
+};
+
+const char *operators[] = {
+  "+", "-", "*", "/", "%",
+  "==", "!=", "<", "<=", ">", ">=",
+  "&&", "||", "!",
+  "&", "|", "~", "^", "<<", ">>",
+  "=", "+=", "-=", "*=", "/=", "%=",
+  "&=", "|=", "^=", "<<=", ">>=",
+};
+
+// get index of a string in string array
+template <typename T, std::size_t N>
+int GetIndex(const char *str, T (&str_array)[N]) {
+  for (int i = 0; i < N; ++i) {
+    if (!strcmp(str, str_array[i])) return i;
+  }
+  return -1;
+}
+
+bool IsOperatorChar(char c) {
+  const char op_chars[] = "+-*/%=!<>&|~^";
+  for (const auto &i : op_chars) {
+    if (i == c) return true;
+  }
+  return false;
+}
+
+}  // namespace
+
+Token Lexer::PrintError(const char *message) {
+  using namespace util;
+  // print error message
+  std::cerr << style("B") << "lexer";
+  std::cerr << " (line " << line_pos_ << "): " << style("RBr") << "error";
+  std::cerr << ": " << message << std::endl;
+  // increase error count
+  ++error_num_;
+  return Token::Error;
+}
+
+int Lexer::ReadEscape() {
+  // eat '\' char
+  NextChar();
+  if (IsEOL()) return -1;
+  switch (last_char_) {
+    case 'a': return '\a';
+    case 'b': return '\b';
+    case 'f': return '\f';
+    case 'n': return '\n';
+    case 'r': return '\r';
+    case 't': return '\t';
+    case 'v': return '\v';
+    case '\\': return '\\';
+    case '\'': return '\'';
+    case '"': return '"';
+    case '0': return '\0';
+    case 'x': {
+      char hex[3] = {0};
+      char *end_pos;
+      // read 2 hex digits
+      for (int i = 0; i < 2; ++i) {
+        NextChar();
+        if (IsEOL()) return -1;
+        hex[i] = last_char_;
+      }
+      // convert to character
+      auto ret = std::strtol(hex, &end_pos, 16);
+      return *end_pos ? -1 : ret;
+    }
+    default: return -1;
+  }
+}
+
+Token Lexer::HandleId() {
+  // read string
+  std::string id;
+  do {
+    id += last_char_;
+    NextChar();
+  } while (!IsEOL() && (std::isalnum(last_char_) || last_char_ == '_'));
+  // check if string is keyword
+  int index = GetIndex(id.c_str(), keywords);
+  if (index < 0) {
+    id_val_ = id;
+    return Token::Id;
+  }
+  else {
+    key_val_ = static_cast<Keyword>(index);
+    return Token::Keyword;
+  }
+}
+
+Token Lexer::HandleNum() {
+  std::string num;
+  auto num_type = NumberType::Normal;
+  // check if is hexadecimal/binary/floating-point number
+  if (last_char_ == '0') {
+    NextChar();
+    switch (last_char_) {
+      // hexadecimal
+      case 'x': case 'X': num_type = NumberType::Hex; break;
+      // binary
+      case 'b': case 'B': num_type = NumberType::Bin; break;
+      default: {
+        if (IsEOL() || !std::isdigit(last_char_)) {
+          // just zero
+          num_val_ = 0;
+          return Token::Num;
+        }
+        else {
+          return PrintError("invalid number");
+        }
+        break;
+      }
+    }
+    NextChar();
+  }
+  // read number string
+  while (!IsEOL() && std::isxdigit(last_char_)) {
+    num += last_char_;
+    NextChar();
+  }
+  // convert to number
+  char *end_pos;
+  switch (num_type) {
+    case NumberType::Hex: {
+      num_val_ = std::strtoul(num.c_str(), &end_pos, 16);
+      break;
+    }
+    case NumberType::Bin: {
+      num_val_ = std::strtoul(num.c_str(), &end_pos, 2);
+      break;
+    }
+    case NumberType::Normal: {
+      num_val_ = std::strtoul(num.c_str(), &end_pos, 10);
+      break;
+    }
+    default:;
+  }
+  // check if conversion is valid
+  return *end_pos ? PrintError("invalid number") : Token::Num;
+}
+
+Token Lexer::HandleString() {
+  std::string str;
+  // start with quotes
+  NextChar();
+  while (last_char_ != '"') {
+    if (last_char_ == '\\') {
+      // read escape char
+      int ret = ReadEscape();
+      if (ret < 0) return PrintError("invalid escape character");
+      str += ret;
+    }
+    else {
+      str += last_char_;
+    }
+    NextChar();
+    if (IsEOL()) return PrintError("expected '\"'");
+  }
+  // eat right quotation mark
+  NextChar();
+  str_val_ = str;
+  return Token::String;
+}
+
+Token Lexer::HandleChar() {
+  // start with quotes
+  NextChar();
+  if (last_char_ == '\\') {
+    // read escape char
+    int ret = ReadEscape();
+    if (ret < 0) return PrintError("invalid escape character");
+    char_val_ = ret;
+  }
+  else {
+    char_val_ = last_char_;
+  }
+  NextChar();
+  // check & eat right quotation mark
+  if (last_char_ != '\'') return PrintError("expected \"'\"");
+  NextChar();
+  return Token::Char;
+}
+
+Token Lexer::HandleOperator() {
+  std::string op;
+  do {
+    op += last_char_;
+    NextChar();
+  } while (!IsEOL() && IsOperatorChar(last_char_));
+  // check if operator is valid
+  int index = GetIndex(op.c_str(), operators);
+  if (index < 0) {
+    return PrintError("unknown operator");
+  }
+  else {
+    op_val_ = static_cast<Operator>(index);
+    return Token::Operator;
+  }
+}
+
+Token Lexer::HandleComment() {
+  // eat '#'
+  NextChar();
+  while (!IsEOL()) NextChar();
+  return NextToken();
+}
+
+Token Lexer::HandleEOL() {
+  do {
+    ++line_pos_;
+    NextChar();
+  } while (IsEOL() && !in_.eof());
+  return NextToken();
+}
+
+Token Lexer::NextToken() {
+  // end of file
+  if (in_.eof()) return Token::End;
+  // skip spaces
+  while (!IsEOL() && std::isspace(last_char_)) NextChar();
+  // skip comment
+  if (last_char_ == '#') return HandleComment();
+  // id or keyword
+  if (std::isalpha(last_char_)) return HandleId();
+  // number
+  if (std::isdigit(last_char_)) return HandleNum();
+  // string
+  if (last_char_ == '"') return HandleString();
+  // character
+  if (last_char_ == '\'') return HandleChar();
+  // operator
+  if (IsOperatorChar(last_char_)) return HandleOperator();
+  // end of line
+  if (IsEOL()) return HandleEOL();
+  // other characters
+  other_val_ = last_char_;
+  NextChar();
+  return Token::Other;
+}
---
+++ src/compiler/front/lexer.h
@@ -0,0 +1,111 @@
+#ifndef TINYLANG_FRONT_LEXER_H_
+#define TINYLANG_FRONT_LEXER_H_
+
+#include <istream>
+#include <string>
+
+namespace tinylang::front {
+
+enum class Token {
+  Error, End,
+  Id, Num, String, Char,
+  Keyword, Operator,
+  Other,
+};
+
+enum class Keyword {
+  // var, let, def, as
+  Var, Let, Def, As,
+  // i32, i8, i32*, i8*
+  Int32, Int8, Int32Ptr, Int8Ptr,
+  // ui32, ui8, ui32*, ui8*
+  UInt32, UInt8, UInt32Ptr, UInt8Ptr,
+  // if, else, while
+  If, Else, While,
+  // break, continue, return
+  Break, Continue, Return,
+};
+
+enum class Operator {
+  // +, -, *, /, %
+  Add, Sub, Mul, Div, Mod,
+  // ==, !=, <, <=, >, >=
+  Equal, NotEqual, Less, LessEqual, Great, GreatEqual,
+  // &&, ||, !
+  LogicAnd, LogicOr, LogicNot,
+  // &, |, ~, ^, <<, >>
+  And, Or, Not, Xor, Shl, Shr,
+  // =, +=, -=, *=, /=, %=
+  Assign, AssAdd, AssSub, AssMul, AssDiv, AssMod,
+  // &=, |=, ^=, <<=, >>=
+  AssAnd, AssOr, AssXor, AssShl, AssShr,
+};
+
+class Lexer {
+ public:
+  Lexer(std::istream &in) : in_(in) { Reset(); }
+
+  // reset status of lexer
+  void Reset() {
+    line_pos_ = 1;
+    error_num_ = 0;
+    last_char_ = ' ';
+    in_ >> std::noskipws;
+  }
+
+  // get next token from input stream
+  Token NextToken();
+
+  // current line position
+  unsigned int line_pos() const { return line_pos_; }
+  // current error count
+  unsigned int error_num() const { return error_num_; }
+  // identifiers
+  const std::string &id_val() const { return id_val_; }
+  // integer values
+  unsigned long long num_val() const { return num_val_; }
+  // string literals
+  const std::string &str_val() const { return str_val_; }
+  // keywords
+  Keyword key_val() const { return key_val_; }
+  // operators
+  Operator op_val() const { return op_val_; }
+  // character literals
+  char char_val() const { return char_val_; }
+  // other characters
+  char other_val() const { return other_val_; }
+
+ private:
+  void NextChar() { in_ >> last_char_; }
+  bool IsEOL() {
+    return in_.eof() || last_char_ == '\n' || last_char_ == '\r';
+  }
+
+  // print error message and return Token::Error
+  Token PrintError(const char *message);
+
+  // read escape character from stream
+  int ReadEscape();
+
+  Token HandleId();
+  Token HandleNum();
+  Token HandleString();
+  Token HandleChar();
+  Token HandleOperator();
+  Token HandleComment();
+  Token HandleEOL();
+
+  std::istream &in_;
+  unsigned int line_pos_, error_num_;
+  char last_char_;
+  // value of token
+  std::string id_val_, str_val_;
+  unsigned int num_val_;
+  Keyword key_val_;
+  Operator op_val_;
+  char char_val_, other_val_;
+};
+
+}  // namespace tinylang::front
+
+#endif  // TINYLANG_FRONT_LEXER_H_
