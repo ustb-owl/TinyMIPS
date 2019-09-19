38ce0f95c517b0acc6f25bf606b2746d8daad7fe
MaxXSoft <x@MaxXSoft.net>
2019-09-19 23:23:11 +0800

    (usedef) added definitions of base classes of SSA

---
+++ src/compiler/define/usedef.cpp
@@ -0,0 +1,19 @@
+#include "define/usedef.h"
+
+using namespace tinylang::define;
+
+void Value::ReplaceBy(const SSAPtr &value) {
+  // TODO: consider optimizing
+  // copy an use list from current value
+  auto uses = uses_;
+  // reroute all uses to new value
+  for (const auto &use : uses) {
+    use->set_value(value);
+  }
+}
+
+void Use::set_value(const SSAPtr &value) {
+  if (value_) value_->RemoveUse(this);
+  value_ = value;
+  if (value_) value_->AddUse(this);
+}
---
+++ src/compiler/define/usedef.h
@@ -0,0 +1,110 @@
+#ifndef TINYLANG_DEFINE_USEDEF_H_
+#define TINYLANG_DEFINE_USEDEF_H_
+
+// reference: LLVM version 1.3
+
+#include <memory>
+#include <utility>
+#include <vector>
+#include <ostream>
+#include <forward_list>
+#include <cstddef>
+#include <cassert>
+
+namespace tinylang::define {
+
+class Value;
+class User;
+class Use;
+
+using SSAPtr = std::shared_ptr<Value>;
+using SSAPtrList = std::vector<SSAPtr>;
+
+// a SSA value
+class Value {
+ public:
+  Value() {}
+  virtual ~Value() = default;
+
+  // add a use reference to current value
+  void AddUse(Use *use) { uses_.push_front(use); }
+  // remove use reference from current value
+  void RemoveUse(Use *use) { uses_.remove(use); }
+  // replace current value by another value
+  void ReplaceBy(const SSAPtr &value);
+  // dump the content of SSA value to output stream
+  virtual void Dump(std::ostream &os) = 0;
+
+  const std::forward_list<Use *> &uses() const { return uses_; }
+
+ private:
+  // singly-linked list for 'Use'
+  std::forward_list<Use *> uses_;
+};
+
+// a bidirectional reference between SSA user and value
+class Use {
+ public:
+  explicit Use(const SSAPtr &value, User *user)
+      : value_(value), user_(user) {}
+  // copy constructor
+  Use(const Use &use)
+      : value_(use.value_), user_(use.user_) {
+    if (value_) value_->AddUse(this);
+  }
+  // move constructor
+  Use(Use &&use) noexcept
+      : value_(std::move(use.value_)), user_(use.user_) {
+    if (value_) {
+      value_->RemoveUse(&use);
+      value_->AddUse(this);
+    }
+  }
+  // destructor
+  ~Use() {
+    if (value_) value_->RemoveUse(this);
+  }
+
+  void set_value(const SSAPtr &value);
+
+  const SSAPtr &value() const { return value_; }
+  User *user() const { return user_; }
+
+ private:
+  SSAPtr value_;
+  User *user_;
+};
+
+// a SSA user which can use other values
+class User : public Value {
+ public:
+  User() : Value() {}
+
+  // preallocate some space for values
+  void reserve(std::size_t size) { operands_.reserve(size); }
+  // add new value to current user
+  void push_back(const SSAPtr &value) {
+    operands_.push_back(Use(value, this));
+  }
+
+  // access value in current user
+  Use &operator[](std::size_t pos) { return operands_[pos]; }
+  // access value in current user (const)
+  const Use &operator[](std::size_t pos) const { return operands_[pos]; }
+
+  // begin iterator
+  auto begin() { return operands_.begin(); }
+  // end iterator
+  auto end() { return operands_.end(); }
+  // count of elements in current user
+  std::size_t size() const { return operands_.size(); }
+  // return true if no value in current user
+  bool empty() const { return operands_.empty(); }
+
+ private:
+  std::vector<Use> operands_;
+};
+
+}  // namespace tinylang::define
+
+#endif  // TINYLANG_DEFINE_USEDEF_H_
