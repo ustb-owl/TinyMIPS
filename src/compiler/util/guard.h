59859167fc0d79ecb0cf4d2442e233695bd7c6be
MaxXSoft <x@MaxXSoft.net>
2019-09-19 18:11:00 +0800

    (guard) added utility class 'Guard'

---
+++ src/compiler/util/guard.h
@@ -0,0 +1,26 @@
+#ifndef TINYLANG_UTIL_GUARD_H_
+#define TINYLANG_UTIL_GUARD_H_
+
+#include <functional>
+#include <utility>
+
+class Guard {
+ public:
+  explicit Guard(std::function<void()> release) : release_(release) {}
+  // move constructor
+  Guard(Guard &&guard) noexcept : release_(std::move(guard.release_)) {}
+  // remove copy constructor
+  Guard(const Guard &) = delete;
+  // destructor
+  ~Guard() { release_(); }
+
+  // remove move operator
+  Guard &operator=(Guard &&) noexcept = delete;
+  // remove copy operator
+  Guard &operator=(const Guard &) = delete;
+
+ private:
+  std::function<void()> release_;
+};
+
+#endif  // TINYLANG_UTIL_GUARD_H_
