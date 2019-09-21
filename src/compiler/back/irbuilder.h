d61ee375edbb3a4df6e5e09a715af0a10aaa9ddd
MaxXSoft <x@MaxXSoft.net>
2019-09-21 17:55:29 +0800

    (ir, irbuilder) added interface definitions of IR and IRBuilder

---
+++ src/compiler/back/irbuilder.h
@@ -0,0 +1,17 @@
+#ifndef TINYLANG_BACK_IRBUILDER_H_
+#define TINYLANG_BACK_IRBUILDER_H_
+
+#include "define/ssa.h"
+
+namespace tinylang::back {
+
+class IRBuilder {
+ public:
+  virtual ~IRBuilder() = default;
+
+  //
+};
+
+}  // namespace tinylang::back
+
+#endif  // TINYLANG_BACK_IRBUILDER_H_
