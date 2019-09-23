51a652ef9cc48d82a3e85a6769b27217d988b3f5
MaxXSoft <x@MaxXSoft.net>
2019-09-23 09:32:09 +0800

    (nested) added utility class 'NestedMap'

---
+++ src/compiler/util/nested.h
@@ -0,0 +1,77 @@
+#ifndef TINYLANG_UTIL_NESTED_H_
+#define TINYLANG_UTIL_NESTED_H_
+
+#include <unordered_map>
+#include <memory>
+
+namespace tinylang::util {
+
+/*
+
+TODO:
+
+complete this utility
+rewrite Environment
+use this to implement 'vars_' in 'TACBuilder'
+rewrite 'label_stacks_'?
+implement 'NewLabel'... etc
+
+*/
+
+template <typename K, typename V>
+class NestedMap;
+
+template <typename K, typename V>
+using NestedMapPtr = std::shared_ptr<NestedMap<K, V>>;
+
+template <typename K, typename V>
+class NestedMap {
+ public:
+  NestedMap() : outer_(nullptr) {}
+  NestedMap(const NestedMapPtr<K, V> &outer) : outer_(outer) {}
+
+  // add item to current map
+  void AddItem(const K &key, const V &value) {
+    map_.insert({key, value});
+  }
+  // get item
+  V GetItem(const K &key, bool recursive) {
+    auto it = map_.find(key);
+    if (it != map_.end()) {
+      return it->second;
+    }
+    else if (outer_ && recursive) {
+      return outer_->GetInfo(key);
+    }
+    else {
+      return nullptr;
+    }
+  }
+  // get item recursively
+  V GetItem(const K &key) { return GetItem(key, true); }
+
+  // outer map
+  const NestedMapPtr<K, V> &outer() const { return outer_; }
+  // check if current map is root map
+  bool is_root() const { return outer_ == nullptr; }
+
+ private:
+  NestedMapPtr<K, V> outer_;
+  std::unordered_map<K, V> map_;
+};
+
+// create a new nested map
+template <typename K, typename V>
+inline NestedMapPtr<K, V> MakeNestedMap() {
+  return std::make_shared<NestedMap<K, V>>();
+}
+
+// create a new nested map (with outer map)
+template <typename K, typename V>
+inline NestedMapPtr<K, V> MakeNestedMap(const NestedMapPtr<K, V> &outer) {
+  return std::make_shared<NestedMap<K, V>>(outer);
+}
+
+}  // namespace tinylang::util
+
+#endif  // TINYLANG_UTIL_NESTED_H_
