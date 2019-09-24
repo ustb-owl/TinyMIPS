4dd9d180672f4beedf96c38a6a9f3d2b89b1ce2a
MaxXSoft <x@MaxXSoft.net>
2019-09-24 21:54:37 +0800

    (genir) updated implementations

--- src/compiler/back/genir.cpp
+++ src/compiler/back/genir.cpp
@@ -5,19 +5,16 @@ using namespace tinylang::define;
 using namespace tinylang::back;
 
 IRPtr VarDefAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   for (const auto &i : defs_) i->GenerateIR(irb);
   return nullptr;
 }
 
 IRPtr LetDefAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   for (const auto &i : defs_) i->GenerateIR(irb);
   return nullptr;
 }
 
 IRPtr FunDefAST::GenerateIR(IRBuilder &irb) {
-  auto guard_type = irb.SetType(type());
   if (body_) {
     // function definition
     auto guard_func = irb.EnterFunction(id_);
@@ -32,14 +29,12 @@ IRPtr FunDefAST::GenerateIR(IRBuilder &irb) {
 }
 
 IRPtr FunCallAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   IRPtrList args;
   for (const auto &i : args_) args.push_back(i->GenerateIR(irb));
   return irb.GenerateFunCall(id_, std::move(args));
 }
 
 IRPtr IfAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   irb.GenerateIfCond(cond_->GenerateIR(irb));
   {
     auto guard = irb.EnterIfTrueBody();
@@ -53,7 +48,6 @@ IRPtr IfAST::GenerateIR(IRBuilder &irb) {
 }
 
 IRPtr WhileAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   {
     auto guard = irb.EnterWhileCond();
     irb.GenerateWhileCond(cond_->GenerateIR(irb));
@@ -66,18 +60,15 @@ IRPtr WhileAST::GenerateIR(IRBuilder &irb) {
 }
 
 IRPtr ControlAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateControl(type_,
                              expr_ ? expr_->GenerateIR(irb) : nullptr);
 }
 
 IRPtr VarElemAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateVarElem(id_, init_ ? init_->GenerateIR(irb) : nullptr);
 }
 
 IRPtr LetElemAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateLetElem(id_, init_->GenerateIR(irb));
 }
 
@@ -86,20 +77,38 @@ IRPtr TypeAST::GenerateIR(IRBuilder &irb) {
 }
 
 IRPtr ArgElemAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateArgElem(id_);
 }
 
 IRPtr BlockAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   for (const auto &i : stmts_) i->GenerateIR(irb);
   return nullptr;
 }
 
 IRPtr BinaryAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
-  return irb.GenerateBinary(op_, lhs_->GenerateIR(irb),
-                            rhs_->GenerateIR(irb));
+  auto guard = irb.SetOprType(lhs_->type(), rhs_->type());
+  if (IsOperatorAssign(op_) && lhs_->IsMemAccess()) {
+    // generate rhs first
+    auto value = rhs_->GenerateIR(irb);
+    if (op_ != Operator::Assign) {
+      auto lhs_ir = lhs_->GenerateIR(irb);
+      value = irb.GenerateBinary(GetDeAssignedOp(op_), lhs_ir, value);
+    }
+    // generate memory store
+    auto guard_store = irb.MarkStore(value);
+    return lhs_->GenerateIR(irb);
+  }
+  else if (op_ == Operator::LogicAnd || op_ == Operator::LogicOr) {
+    // generate logical operation
+    irb.GenerateLogicLHS(op_, lhs_->GenerateIR(irb));
+    auto guard_logic = irb.EnterLogicRHS(op_);
+    return irb.GenerateLogicRHS(rhs_->GenerateIR(irb));
+  }
+  else {
+    // just binary operation
+    return irb.GenerateBinary(op_, lhs_->GenerateIR(irb),
+                              rhs_->GenerateIR(irb));
+  }
 }
 
 IRPtr CastAST::GenerateIR(IRBuilder &irb) {
@@ -107,38 +116,36 @@ IRPtr CastAST::GenerateIR(IRBuilder &irb) {
 }
 
 IRPtr UnaryAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
+  auto guard = irb.SetOprType(opr_->type(), nullptr);
   return irb.GenerateUnary(op_, opr_->GenerateIR(irb));
 }
 
 IRPtr IdAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateId(id_);
 }
 
 IRPtr NumAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateNum(num_);
 }
 
 IRPtr StringAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateString(str_);
 }
 
 IRPtr CharAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
   return irb.GenerateChar(c_);
 }
 
 IRPtr ArrayAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
+  // NOTE: operand type is type of current array (pointer type)
+  auto guard = irb.SetOprType(type(), nullptr);
   IRPtrList elems;
   for (const auto &i : elems_) elems.push_back(i->GenerateIR(irb));
   return irb.GenerateArray(std::move(elems));
 }
 
 IRPtr IndexAST::GenerateIR(IRBuilder &irb) {
-  auto guard = irb.SetType(type());
+  // NOTE: lhs type is type of current expression, rhs type is type of index
+  auto guard = irb.SetOprType(type(), index_->type());
   return irb.GenerateIndex(id_, index_->GenerateIR(irb));
 }
