f06b8186ea23af247b4fe9ab8ee9b0948850ea58
MaxXSoft <x@MaxXSoft.net>
2019-10-08 22:57:12 +0800

    (version) added header 'version.h'

---
+++ src/compiler/version.h
@@ -0,0 +1,26 @@
+#ifndef TINYLANG_VERSION_H_
+#define TINYLANG_VERSION_H_
+
+// version information of Ionia
+
+#ifndef APP_NAME
+#define APP_NAME            "TinyLang"
+#endif
+
+#ifndef APP_VERSION
+#define APP_VERSION         "0.0.0"
+#endif
+
+#ifndef APP_VERSION_MAJOR
+#define APP_VERSION_MAJOR   0
+#endif
+
+#ifndef APP_VERSION_MINOR
+#define APP_VERSION_MINOR   0
+#endif
+
+#ifndef APP_VERSION_PATCH
+#define APP_VERSION_PATCH   0
+#endif
+
+#endif  // TINYLANG_VERSION_H_
