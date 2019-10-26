bad77f85c5b7b17ed476daac930b354b070bed8d
MaxXSoft <x@MaxXSoft.net>
2019-10-26 20:59:05 +0800

    (tb) added macro 'ASSERT'

--- test/cpu/include/tb.v
+++ test/cpu/include/tb.v
@@ -3,21 +3,30 @@
 
 `define TICK  _tb_tick
 
-`define GEN_TICK(clk, rst)              \
-    integer `TICK;                      \
-    always @(posedge clk) begin         \
-      if (rst) begin                    \
-        `TICK <= 0;                     \
-      end                               \
-      else begin                        \
-        `TICK <= `TICK + 1;             \
-      end                               \
+`define GEN_TICK(clk, rst)                                        \
+    integer `TICK;                                                \
+    always @(posedge clk) begin                                   \
+      if (rst) begin                                              \
+        `TICK <= 0;                                               \
+      end                                                         \
+      else begin                                                  \
+        `TICK <= `TICK + 1;                                       \
+      end                                                         \
     end
 
-`define DISPLAY(name, val)              \
+`define DISPLAY(name, val)                                        \
     $display("[%8h] %s = 0x%8h", `TICK, name, val)
 
-`define END_AT_TICK(tick)               \
+`define ASSERT(name, val, tick, expect)                           \
+    if ((tick) == `TICK && (val) != (expect)) begin               \
+      $display("[%8h] ERROR! Assertion failed at signal '%s', ",  \
+               `TICK, name,                                       \
+               "expected = 0x%8h, actual = 0x%8h",                \
+               expect, val);                                      \
+      $finish;                                                    \
+    end                                                           \
+
+`define END_AT_TICK(tick)                                         \
     if (`TICK >= tick) $finish
 
 `endif  // OOPA_TEST_INCLUDE_TB_V_
