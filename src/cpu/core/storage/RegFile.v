06657e681b3b49ef09832702d5e4619bf784c5d0
418teapot <scp000682@gmail.com>
2019-10-28 20:12:59 +0800

    (RegFile) moved declaration of 'integer i' to outside

--- src/cpu/core/storage/RegFile.v
+++ src/cpu/core/storage/RegFile.v
@@ -20,11 +20,11 @@ module RegFile(
 );
 
   reg[`DATA_BUS] registers[0:31];
+  integer i;
 
   // writing
   always @(posedge clk) begin
     if (rst) begin
-      integer i;
       for (i = 0; i < 32; i = i + 1) begin
         registers[i] <= 0;
       end
